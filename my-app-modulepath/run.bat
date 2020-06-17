f9a4d0214dfd764f903557671db9455fa918fd80
Sebastien Rinsoz <sebastien@yoctopuce.com>
2020-06-17 09:13:12 +0200

    cleanup

--- my-app-modulepath/run.bat
+++ my-app-modulepath/run.bat
@@ -1,7 +1,6 @@
-#!/bin/sh
+@echo OFF
 javac -d outDir --module-source-path Sources --module-path ../YoctoLib.java.40924/Binaries/yoctoAPI.jar Sources/main.app/com/mycompany/app/App.java
 javac -d outDir --module-source-path Sources --module-path ../YoctoLib.java.40924/Binaries/yoctoAPI.jar Sources/hello.module/com/mycompany/module/Hello.java
-
 java --module-path outDir:../YoctoLib.java.40924/Binaries/yoctoAPI.jar -m main.app/com.mycompany.app.App
 
 
