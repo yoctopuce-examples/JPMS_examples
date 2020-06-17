1a4c2ffad9019bce3d50729661cc0ad6a69084ee
Sebastien Rinsoz <sebastien@yoctopuce.com>
2020-06-17 09:05:36 +0200

    rename some dir

--- my-app-classpath/run.bat
+++ my-app-classpath/run.bat
@@ -1,5 +1,4 @@
 @echo OFF
-@echo OFF
 javac -d classes -cp ..\YoctoLib.java.40924\Binaries\yoctoAPI.jar src\com\mycompany\app\App.java
 java -cp ..\YoctoLib.java.40924\Binaries\yoctoAPI.jar:classes com.mycompany.app.App
 
